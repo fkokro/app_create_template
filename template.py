e228873d266fc935687c69d5067eabb9b8ae5788
Flomo Kokro <fkokro@yahoo.com>
2024-04-20 02:47:51 -0400

    Framework created

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
 
-project_name = 'App Template'
+project_name = 'AppTemplate'
 
 list_of_files = [
     ".github/workflows/.gitkeep/",
@@ -31,4 +31,17 @@ list_of_files = [
 
 for filepath in list_of_files:
     filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    
+    if not os.path.exists(filedir) and filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory {filedir} for the file: {filename}")
+    
+    if not (os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file {filepath}")
+            
+    else:
+        logging.info(f"File {filepath} already exists")
     
